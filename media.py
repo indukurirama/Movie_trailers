8524babb3ed2555097347349720c3d8487a74424
indukurirama <38848888+indukurirama@users.noreply.github.com>
2018-05-21 11:44:20 +0530

    updated

--- media.py
+++ media.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python
-print("content-type:text/html \r")
 import webbrowser
+
+
 class Movie():
-    VALID_RATINGS = ["EXCELLENT","GOOD","BAD","AVERAGE"]
-    def __init__(self,cineMax,cineLine,cinePostImage,cineTube):
-        self.title=cineMax
-        self.storyline=cineLine
+    '''
+    class Movie():
+    In this Class Movie set of attributes are declared to run
+    movie trailer project.
+
+    Attributes:
+    attr1 (cineMax): Title of the movie.
+    attr2 (cineLine): About the story line of the movie.
+    attr3 (cinePostImage): Poster image of the movie.
+    attr4 (cineTube): About youtube link  of the movie trailer.
+
+    '''
+
+    VALID_RATINGS = ["EXCELLENT", "GOOD", "BAD", "AVERAGE"]
+
+    def __init__(self, cineMax, cineLine, cinePostImage, cineTube):
+        """ cineMax, cineLine, cinePostImage, cineTube all
+        these are attributes """
+
+        self.title = cineMax
+        self.storyline = cineLine
         self.poster_image_url = cinePostImage
         self.trailer_youtube_url = cineTube
+
     def show_trailer(self):
+        ''''It runs movie trailer project on your favourite browser.'''
+
         webbrowser.open(self.trailer_youtube_url)
